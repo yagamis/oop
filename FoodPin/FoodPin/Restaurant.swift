6999c8029c669cdb7aa301c9ee144b24769fb1bd
yagamis <xiaobo@xiaobodeMacBook-Pro.local>
2015-10-10 17:13:03 +0800

    完成

---
+++ FoodPin/FoodPin/Restaurant.swift
@@ -0,0 +1,21 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by xiaobo on 15/10/10.
+//  Copyright © 2015年 xiaobo. All rights reserved.
+//
+
+import Foundation
+
+
+struct Restaurant {
+    var name :String
+    var type :String
+    var location :String
+    var image :String
+    var isVisted :Bool
+}
+
+
+//let r = Restaurant(name: <#T##String#>, type: <#T##String#>, location: <#T##String#>, image: <#T##String#>, isVisted: <#T##Bool#>)
